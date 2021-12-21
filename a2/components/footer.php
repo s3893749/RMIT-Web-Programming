cc06cdb63df4d26c0e65365a9bd940758c5f6f43
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-12-21 19:35:45 +1100

    - Created footer component to store all footer data

---
+++ a2/components/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="content">
+    <div>&copy;<script>
+            document.write(new Date().getFullYear());
+        </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+    <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
+    <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
+    </div>
+</footer>
