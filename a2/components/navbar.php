b1f5e0bb0ab930e9f31d7180d53d22255ee14dbf
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-12-21 19:35:17 +1100

    - Created navbar.php component storing all navbar HTML

---
+++ a2/components/navbar.php
@@ -0,0 +1,11 @@
+<nav>
+    <ol>
+        <li><a href="#" class="flex-row-center">About Us</a></li>
+        <li><a href="#" class="flex-row-center">Seats & Prices</a></li>
+        <li><a href="#" class="flex-row-center">Now Showing</a></li>
+        <li class="nav-spacer"></li>
+        <li><a href="#"  class="flex-row-center bold"><img src="./svg/ticket.svg" alt="Ticket Icon">Book</a></li>
+        <li class="nav-spacer"></li>
+        <li><a href="#" class="flex-row-center bold"><img src="./svg/user.svg" alt="User Icon">Login</a></li>
+    </ol>
+</nav>
