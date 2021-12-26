1fba597cdc838e21102b5db6e79df9ad32ba004d
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-12-26 22:09:24 +1100

    - created new debugbar php component

---
+++ a2/components/debugbar.php
@@ -0,0 +1,12 @@
+<aside id="debug">
+    <hr>
+    <h3>Debug Area</h3>
+    <pre>
+GET Contains:
+<?php print_r($_GET) ?>
+POST Contains:
+<?php print_r($_POST) ?>
+SESSION Contains:
+<?php print_r($_SESSION) ?>
+      </pre>
+</aside>
