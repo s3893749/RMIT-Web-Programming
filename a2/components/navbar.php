99d78f0b22c6226df158608cf634d964b5d9f71a
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-12-26 22:09:09 +1100

    - updated navbar links to point to the correct page. - updated images to use the correct ../../media directory

--- a2/components/navbar.php
+++ a2/components/navbar.php
@@ -1,12 +1,12 @@
 <nav>
     <ol>
-        <li><a href="#" class="flex-core">About Us</a></li>
-        <li><a href="#" class="flex-core">Seats & Prices</a></li>
-        <li><a href="#" class="flex-core">Now Showing</a></li>
+        <li><a href="./#about-us" class="flex-core">About Us</a></li>
+        <li><a href="./#seats-and-prices" class="flex-core">Seats & Prices</a></li>
+        <li><a href="./#now-showing" class="flex-core">Now Showing</a></li>
         <li class="nav-spacer"></li>
-        <li><a href="#"  class="flex-core bold flex-attribute-align-center flex-attribute-spaced-around"><img src="./svg/ticket.svg" alt="Ticket Icon">Book</a></li>
+        <li><a href="./booking.php"  class="flex-core bold flex-attribute-align-center flex-attribute-spaced-around"><img src="../../media/svg/ticket.svg" alt="Ticket Icon">Book</a></li>
         <li class="nav-spacer"></li>
-        <li><a href="#" class="flex-core bold flex-attribute-align-center flex-attribute-spaced-around"><img src="./svg/user.svg" alt="User Icon">Login</a></li>
+        <li><a href="#" class="flex-core bold flex-attribute-align-center flex-attribute-spaced-around"><img src="../../media/svg/user.svg" alt="User Icon">Login</a></li>
     </ol>
 </nav>
 
@@ -14,6 +14,6 @@
         <a href="#">About Us</a>
         <a href="#">Seats & Prices</a>
         <a href="#">Now Showing</a>
-        <a href="#"><img src="./svg/ticket.svg" alt="Ticket Icon"><p class="bold">Book</p></a>
-        <a href="#"><img src="./svg/user.svg" alt="User Icon"><p class="bold">Login</p></a>
+        <a href="#"><img src="../../media/svg/user.svg" alt="Ticket Icon"><p class="bold">Book</p></a>
+        <a href="#"><img src="../../media/svg/ticket.svg" alt="User Icon"><p class="bold">Login</p></a>
 </div>
