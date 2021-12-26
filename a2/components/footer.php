e7964bac14cb9bbacb35c50fee19a7728cfe00d8
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-12-26 22:08:18 +1100

    - updated footer links to correct point back to the index page

--- a2/components/footer.php
+++ a2/components/footer.php
@@ -6,7 +6,7 @@
         <section id="footer-main-content">
             <div id="company-section">
                 <div>
-                    <img src="./svg/logo.svg" alt="Luncardo Cinema Logo">
+                    <img src="../../media/svg/logo.svg" alt="Luncardo Cinema Logo">
                     <h1>Luncardo Cinemas</h1>
                 </div>
                 <p>ABN: 40 123 456 789</p>
@@ -15,15 +15,15 @@
             </div>
             <div id="links">
                 <p class="bold">Links:</p>
-                <a href="#landing">Landing</a>
-                <a href="#about-us">About Us</a>
-                <a href="#seats-and-prices">Seats and Prices</a>
-                <a href="#now-showing">Now Showing</a>
+                <a href="./#landing">Landing</a>
+                <a href="./#about-us">About Us</a>
+                <a href="./#seats-and-prices">Seats and Prices</a>
+                <a href="./#now-showing">Now Showing</a>
                 <a href="booking.php">Bookings</a>
             </div>
             <div id="improvement">
                 <a href="mailto:s3893749@student.rmit.edu.au">
-                <img src="./svg/chat_icon.svg" alt="Chat Icon SVG">
+                <img src="../../media/svg/chat_icon.svg" alt="Chat Icon SVG">
                 <p>Tell us how we could <br> improve our site!</p>
                 </a>
             </div>
