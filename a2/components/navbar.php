306bc3efde9d4f3d22c8bd2e27eed923d5b0e88e
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-12-26 16:55:07 +1100

    - Created navbar.php component, this is required into the core document before being send to the users browser.

--- a2/components/navbar.php
+++ a2/components/navbar.php
@@ -1,11 +1,11 @@
 <nav>
     <ol>
-        <li><a href="#" class="flex-row-center">About Us</a></li>
-        <li><a href="#" class="flex-row-center">Seats & Prices</a></li>
-        <li><a href="#" class="flex-row-center">Now Showing</a></li>
+        <li><a href="#" class="flex-core">About Us</a></li>
+        <li><a href="#" class="flex-core">Seats & Prices</a></li>
+        <li><a href="#" class="flex-core">Now Showing</a></li>
         <li class="nav-spacer"></li>
-        <li><a href="#"  class="flex-row-center bold"><img src="./svg/ticket.svg" alt="Ticket Icon">Book</a></li>
+        <li><a href="#"  class="flex-core bold flex-attribute-align-center flex-attribute-spaced-around"><img src="./svg/ticket.svg" alt="Ticket Icon">Book</a></li>
         <li class="nav-spacer"></li>
-        <li><a href="#" class="flex-row-center bold"><img src="./svg/user.svg" alt="User Icon">Login</a></li>
+        <li><a href="#" class="flex-core bold flex-attribute-align-center flex-attribute-spaced-around"><img src="./svg/user.svg" alt="User Icon">Login</a></li>
     </ol>
 </nav>
