7d97031a82c73b71764cbc283a20f55503685a72
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-12-26 21:51:42 +1100

    - added mobile menu to navbar.php component

--- a2/components/navbar.php
+++ a2/components/navbar.php
@@ -9,3 +9,11 @@
         <li><a href="#" class="flex-core bold flex-attribute-align-center flex-attribute-spaced-around"><img src="./svg/user.svg" alt="User Icon">Login</a></li>
     </ol>
 </nav>
+
+<div class="mobile-menu">
+        <a href="#">About Us</a>
+        <a href="#">Seats & Prices</a>
+        <a href="#">Now Showing</a>
+        <a href="#"><img src="./svg/ticket.svg" alt="Ticket Icon"><p class="bold">Book</p></a>
+        <a href="#"><img src="./svg/user.svg" alt="User Icon"><p class="bold">Login</p></a>
+</div>
