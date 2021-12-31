4f4f1c9ae88e2e1f3e71ac13e94b38ca6484d798
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-12-31 16:23:54 +1100

    - added alternative light icons to the navbar, these are switched by the sticky css class.

--- a2/components/navbar.php
+++ a2/components/navbar.php
@@ -4,9 +4,9 @@
         <li><a href="./#seats-and-prices" class="flex-core">Seats & Prices</a></li>
         <li><a href="./#now-showing" class="flex-core">Now Showing</a></li>
         <li class="nav-spacer"></li>
-        <li><a href="./booking.php"  class="flex-core bold flex-attribute-align-center flex-attribute-spaced-around"><img src="../../media/svg/ticket.svg" alt="Ticket Icon">Book</a></li>
+        <li><a href="./booking.php"  class="flex-core bold flex-attribute-align-center flex-attribute-spaced-around"><img src="../../media/svg/ticket.svg" alt="Ticket Icon Dark" class="icon-dark"><img src="../../media/svg/ticket-light.svg" alt="Ticket Icon Light" class="icon-light">Book</a></li>
         <li class="nav-spacer"></li>
-        <li><a href="#" class="flex-core bold flex-attribute-align-center flex-attribute-spaced-around"><img src="../../media/svg/user.svg" alt="User Icon">Login</a></li>
+        <li><a href="#" class="flex-core bold flex-attribute-align-center flex-attribute-spaced-around"><img src="../../media/svg/user.svg" alt="User Icon Dark" class="icon-dark"><img src="../../media/svg/user-light.svg" alt="User Icon Light" class="icon-light">Login</a></li>
     </ol>
 </nav>
 
