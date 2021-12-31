ec17706ecc1e4e2c738365337323f6eb5a9239dd
Jack Harris <79836947+s3893749@users.noreply.github.com>
2021-12-31 16:22:38 +1100

    - Copied navbar.js from W3 schools https://www.w3schools.com/howto/howto_js_navbar_sticky.asp

---
+++ a2/javascript/navbar.js
@@ -0,0 +1,20 @@
+//Navbar script createed by W3 schools https://www.w3schools.com/howto/howto_js_navbar_sticky.asp
+//Not created by JACK HARRIS, accessed and copied on 31/12/2021
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("header-nav-container");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        navbar.classList.add("header-nav-container-sticky")
+    } else {
+        navbar.classList.remove("header-nav-container-sticky");
+    }
+}
