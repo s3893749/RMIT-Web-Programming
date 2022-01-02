997c55cf7891f50c1f72c38d5fffed297f7c0399
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-01-02 15:04:19 +1100

    - removed unused links for navbar and mobile navbar

--- a2/components/navbar.php
+++ a2/components/navbar.php
@@ -1,20 +1,17 @@
 <nav>
-    <a href="#" id="mobile-toggle"><img src="../../media/svg/mobile-toggle.svg" alt="Mobile Menu Icon Light" class="icon-light"></a>
+    <a href="javascript:showMobileMenu();" id="mobile-toggle"><img src="../../media/svg/mobile-toggle.svg" alt="Mobile Menu Icon Light" class="icon-light"></a>
     <ol>
         <li><a href="./#about-us" class="flex-core">About Us</a></li>
         <li><a href="./#seats-and-prices" class="flex-core">Seats & Prices</a></li>
         <li><a href="./#now-showing" class="flex-core">Now Showing</a></li>
         <li class="nav-spacer"></li>
-        <li><a href="./booking.php"  class="flex-core bold flex-attribute-align-center flex-attribute-spaced-around"><img src="../../media/svg/ticket.svg" alt="Ticket Icon Dark" class="icon-dark"><img src="../../media/svg/ticket-light.svg" alt="Ticket Icon Light" class="icon-light">Book</a></li>
-        <li class="nav-spacer"></li>
-        <li><a href="#" class="flex-core bold flex-attribute-align-center flex-attribute-spaced-around"><img src="../../media/svg/user.svg" alt="User Icon Dark" class="icon-dark"><img src="../../media/svg/user-light.svg" alt="User Icon Light" class="icon-light">Login</a></li>
+        <li><a href="./booking.php"  class="flex-core bold flex-attribute-align-center flex-attribute-spaced-around"><img src="../../media/svg/ticket.svg" alt="Ticket Icon Dark" class="icon-dark"><img src="../../media/svg/ticket-light.svg" alt="Ticket Icon Light" class="icon-light">Bookings</a></li>
     </ol>
 </nav>
 
-<div class="mobile-menu">
-        <a href="#">About Us</a>
-        <a href="#">Seats & Prices</a>
-        <a href="#">Now Showing</a>
-        <a href="#"><img src="../../media/svg/user.svg" alt="Ticket Icon"><p class="bold">Book</p></a>
-        <a href="#"><img src="../../media/svg/ticket.svg" alt="User Icon"><p class="bold">Login</p></a>
+<div class="mobile-menu" id="mobile-menu">
+        <a href="./#about-us">About Us</a>
+        <a href="./#seats-and-prices">Seats & Prices</a>
+        <a href="./#now-showing">Now Showing</a>
+        <a href="./booking.php"><img src="../../media/svg/user.svg" alt="Ticket Icon"><p class="bold">Bookings</p></a>
 </div>
