56d3689d611ab93f8311f231b95e59e7d4b420a1
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-01-04 19:30:49 +1100

    - created new console class with static log function

---
+++ a2/components/Console.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Copyright Jack Harris
+ * Peninsula Interactive - RMIT_Web_Programming
+ * Last Updated - 4/01/2022
+ */
+
+namespace components;
+
+class Console
+{
+    public static function log($output){
+        if(is_array($output)){
+            $output = json_encode($output);
+        }else if(!is_int($output) || !is_string($output)){
+            $output = var_dump($output);
+        }
+
+        echo "<script>console.log($output)</script>";
+    }
+
+}
