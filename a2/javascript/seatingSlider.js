d8398a53487c07da4ac1625ab893230402cfde17
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-01-06 18:53:53 +1100

    - created seatingSlider.js file to toggle between all the different seat types if a photo is present for them in the seating.json file.

---
+++ a2/javascript/seatingSlider.js
@@ -0,0 +1,26 @@
+window.addEventListener("DOMContentLoaded",function (){
+
+    const slider = document.getElementById("imageSlider");
+    const slides = slider.children;
+
+    let slideCounter = 0;
+
+    slides[0].style.display = "block";
+
+    setInterval(()=>{
+        slides[slideCounter].style.display = "none";
+
+        if(slideCounter >= slides.length-1){
+            slideCounter = 0;
+        }else {
+            slideCounter++;
+        }
+
+        slides[slideCounter].style.display = "block";
+    },5000)
+
+
+    console.log(slides);
+
+})
+
