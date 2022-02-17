0b8aecd713027fe7fe5f55bb67d75cc61e65743b
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-02-17 23:21:38 +1100

    - created placeholder confirmation.php view

---
+++ a3/app/views/confirmation.php
@@ -0,0 +1,18 @@
+<?php require_once VIEWS . "components/head.php" ?>
+<body>
+
+<div id="background"></div>
+
+
+<!-- Main Page Content -->
+<main>
+    <section id="about-us">
+        <h2>About Us</h2>
+        <?php var_dump($_SESSION["booking"]);?>
+
+    </section>
+
+
+</main>
+
+<?php include VIEWS."components/footer.php"?>
