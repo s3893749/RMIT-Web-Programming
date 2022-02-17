c2ce9d0a363d220a1b1e1d0572c7ed11cc278692
Jack Harris <79836947+s3893749@users.noreply.github.com>
2022-02-17 23:30:02 +1100

    - imported Seat class from A2

---
+++ a3/app/models/Seat.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Copyright Jack Harris
+ * Peninsula Interactive - RMIT_Web_Programming
+ * Last Updated - 6/01/2022
+ */
+
+namespace app\models;
+
+class Seat
+{
+    private string $type;
+    private int $price;
+    private int $discountPrice;
+    private string $code;
+    private string $image;
+    private array $discountTimes;
+
+    public function __construct(array $seatData){
+        $this->type = $seatData["type"];
+        $this->price = $seatData["price"];
+        $this->discountPrice = $seatData["discounted-price"];
+        $this->code = $seatData["code"];
+        if($seatData["photo"] != null){
+            $this->image = $seatData["photo"];
+
+        }
+        $this->discountTimes = $seatData["discount-times"];
+
+    }
+
+    public function renderType(string $style = null){
+        if($style == "h1"){
+            echo "<h1>$this->type</h1>";
+        }else if($style == "h2"){
+            echo "<h2>$this->type</h2>";
+        }else if($style == "h3"){
+            echo "<h3>$this->type</h3>";
+        }else if($style == "h4"){
+            echo "<h4>$this->type</h4>";
+        }else if($style == "h5"){
+            echo "<h5>$this->type</h5>";
+        }else{
+            echo "<p>$this->type</p>";
+        }
+    }
+
+    public function getCode(){
+        return $this->code;
+    }
+
+    public function getName(){
+        return $this->type;
+    }
+
+    public function renderPrice(){
+        echo "<p>$ $this->price</p>";
+    }
+
+    public function getPrice(){
+        return $this->price;
+    }
+
+    public function getDiscountedPrice(){
+        return $this->discountPrice;
+    }
+
+    public function renderDiscountTimes(){
+        foreach ($this->discountTimes as $discountTime){
+            //echo "$discountTime\n";
+        }
+
+    }
+
+    public function getDiscountTimes(bool $json = false){
+        if($json){
+            $return = json_encode($this->discountTimes);
+        }else{
+            $return = $this->discountTimes;
+        }
+        return $return;
+    }
+
+    public function renderDiscountedPrice(){
+        echo "<p>$this->discountPrice</p>";
+    }
+
+    public function hasImage(): bool{
+
+        if(isset($this->image)) {
+            $outcome = true;
+        }else{
+            $outcome = false;
+        }
+
+        return $outcome;
+    }
+
+    public function renderImage(){
+        if(isset($this->image)) {
+            echo "<img src='$this->image' alt='$this->type Seating Photo'>";
+        }
+    }
+
+
+
+}
